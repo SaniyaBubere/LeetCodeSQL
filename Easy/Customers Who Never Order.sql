42878280e33271491f64738568b3193741252d01
Saniya Bubere <116152917+SaniyaBubere@users.noreply.github.com>
2023-08-30 17:15:24 +0530

    Create Customers Who Never Order.sql

---
+++ Easy/Customers Who Never Order.sql
@@ -0,0 +1,38 @@
+/*Table: Customers
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
++-------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table indicates the ID and name of a customer.
+ 
+
+Table: Orders
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| customerId  | int  |
++-------------+------+
+id is the primary key (column with unique values) for this table.
+customerId is a foreign key (reference columns) of the ID from the Customers table.
+Each row of this table indicates the ID of an order and the ID of the customer who ordered it.
+ 
+
+Write a solution to find all customers who never order anything.
+
+Return the result table in any order.*/
+
+-- Solution
+
+-- Selecting customer names from the 'customers' table
+SELECT name AS Customers
+FROM customers c
+-- Left joining 'customers' with 'orders' on customer_id
+LEFT JOIN orders o ON c.id = o.Customerid
+-- Filtering the result to include only customers who never placed an order
+WHERE o.customerID IS NULL;
