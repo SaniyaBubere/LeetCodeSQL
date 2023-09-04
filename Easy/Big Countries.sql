13e857801e48bab4b68ac52b2095f1790560729b
Saniya Bubere <116152917+SaniyaBubere@users.noreply.github.com>
2023-09-04 17:43:17 +0530

    Create  Big Countries.sql

---
+++ Easy/Big Countries.sql
@@ -0,0 +1,59 @@
+/*Table: World
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| name        | varchar |
+| continent   | varchar |
+| area        | int     |
+| population  | int     |
+| gdp         | bigint  |
++-------------+---------+
+name is the primary key (column with unique values) for this table.
+Each row of this table gives information about the name of a country, the continent to which it belongs, its area, the population, and its GDP value.
+ 
+
+A country is big if:
+
+it has an area of at least three million (i.e., 3000000 km2), or
+it has a population of at least twenty-five million (i.e., 25000000).
+Write a solution to find the name, population, and area of the big countries.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+World table:
++-------------+-----------+---------+------------+--------------+
+| name        | continent | area    | population | gdp          |
++-------------+-----------+---------+------------+--------------+
+| Afghanistan | Asia      | 652230  | 25500100   | 20343000000  |
+| Albania     | Europe    | 28748   | 2831741    | 12960000000  |
+| Algeria     | Africa    | 2381741 | 37100000   | 188681000000 |
+| Andorra     | Europe    | 468     | 78115      | 3712000000   |
+| Angola      | Africa    | 1246700 | 20609294   | 100990000000 |
++-------------+-----------+---------+------------+--------------+
+Output: 
++-------------+------------+---------+
+| name        | population | area    |
++-------------+------------+---------+
+| Afghanistan | 25500100   | 652230  |
+| Algeria     | 37100000   | 2381741 |
++-------------+------------+---------+ */
+
+-- Solution
+
+-- Selecting the name, population, and area from the 'world' table
+SELECT name, population, area
+
+-- Filtering the rows where either area is greater than or equal to 3,000,000 square kilometers
+-- or population is greater than or equal to 25,000,000 people
+FROM world
+
+-- The WHERE clause specifies the conditions for filtering
+WHERE area >= 3000000 OR population >= 25000000;
