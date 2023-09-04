1672d17341483a769b439857e20bc930fbc949e1
Saniya Bubere <116152917+SaniyaBubere@users.noreply.github.com>
2023-09-04 16:46:41 +0530

    Create Rising Temperature.sql

---
+++ Easy/Rising Temperature.sql
@@ -0,0 +1,52 @@
+/* Table: Weather
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| recordDate    | date    |
+| temperature   | int     |
++---------------+---------+
+id is the column with unique values for this table.
+This table contains information about the temperature on a certain day.
+ 
+
+Write a solution to find all dates' Id with higher temperatures compared to its previous dates (yesterday).
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Weather table:
++----+------------+-------------+
+| id | recordDate | temperature |
++----+------------+-------------+
+| 1  | 2015-01-01 | 10          |
+| 2  | 2015-01-02 | 25          |
+| 3  | 2015-01-03 | 20          |
+| 4  | 2015-01-04 | 30          |
++----+------------+-------------+
+Output: 
++----+
+| id |
++----+
+| 2  |
+| 4  |
++----+
+Explanation: 
+In 2015-01-02, the temperature was higher than the previous day (10 -> 25).
+In 2015-01-04, the temperature was higher than the previous day (20 -> 30).*/
+
+-- Solution
+
+-- Selecting the IDs of dates with higher temperatures compared to their previous dates (yesterday)
+SELECT t1.id AS Id
+FROM wether AS t1  -- Alias the Weather table as t1
+LEFT JOIN wether AS t2 ON DateDiff(t1.recordDate, t2.recordDate) = 1  -- Self-join to compare with the previous day
+WHERE t2.temperature IS NOT NULL  -- Ensure there is previous day's data to compare
+AND t1.temprature > t2.temprature;  -- Filter for higher temperatures than the previous day
