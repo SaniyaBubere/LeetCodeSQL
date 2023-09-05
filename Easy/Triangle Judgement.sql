7c58ef9e4c9f4d84d3e42fa935e309fbd7374bf1
Saniya Bubere <116152917+SaniyaBubere@users.noreply.github.com>
2023-09-05 15:41:34 +0530

    Create Triangle Judgement.sql

---
+++ Easy/Triangle Judgement.sql
@@ -0,0 +1,49 @@
+/*Table: Triangle
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| x           | int  |
+| y           | int  |
+| z           | int  |
++-------------+------+
+In SQL, (x, y, z) is the primary key column for this table.
+Each row of this table contains the lengths of three line segments.
+ 
+
+Report for every three line segments whether they can form a triangle.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Triangle table:
++----+----+----+
+| x  | y  | z  |
++----+----+----+
+| 13 | 15 | 30 |
+| 10 | 20 | 15 |
++----+----+----+
+Output: 
++----+----+----+----------+
+| x  | y  | z  | triangle |
++----+----+----+----------+
+| 13 | 15 | 30 | No       |
+| 10 | 20 | 15 | Yes      |
++----+----+----+----------+ */
+
+-- Solution
+
+-- Selecting three columns x, y, and z from the 'triangle' table
+SELECT x, y, z,
+
+-- Using the IF() function to check if the three values form a valid triangle
+IF(x + y > z AND y + z > x AND x + z > y, 'Yes', 'No') AS Triangle
+
+-- From the 'triangle' table
+FROM triangle;
